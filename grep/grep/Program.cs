21f87bd34f2133b6c59245c63928e4ac0f4220e8
chazeftw <rasmusloejee@gmail.com>
2015-12-29 02:26:18 +0100

    Initial, basic works

---
+++ grep/grep/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Read each line of the file into a string array. Each element
+            // of the array is one line of the file.
+            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\chaze\Desktop\grep.txt");
+
+            string text = Console.ReadLine();
+
+            // Display the file contents by using a foreach loop.
+            Console.WriteLine("The word " + text + " is found on the following lines:");
+            foreach (string line in lines)
+            {
+                // Use a tab to indent each line of the file.
+                if(line.IndexOf(text) != -1)
+                {
+                    Console.WriteLine("\t" + (line.IndexOf(text)+1));
+                }
+            }
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+        }
+    }
+}
